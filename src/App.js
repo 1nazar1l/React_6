0724d156c3972319bee78892748ef990fae19752
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-05-21 20:16:46 +0300

    add code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import CityComponent from './components/CityComponent';
+import WeatherComponent from './components/WeatherComponent'
+import { useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState()
+
+  async function fetchWeather(event){
+    event.preventDefault()
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`)
+    let data = await response.json()
+    setWeather(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <span className='AppLabel'>
+        React Weather App
+      </span>
+      {city && weather ?
+      (<WeatherComponent weather={weather}/>) :
+      (<CityComponent setCity={setCity} fetchWeather={fetchWeather}/>)
+      }
     </div>
   );
 }
